7e222cce12397d08c39f94dc5c822375a3d871c7
Bacs2009 <Bacs@ua.fm>
2014-03-04 17:07:14 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta name="description" content="Портфолио дизайнера Евгении Панченко. Все виды сайтов, открытки, визити, логотипы, фирменный стиль и многое другое">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Портфолио</title>
+<link href="" rel="shortcut icon" type="image/x-icon" />
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+<link href="css/style_other.css" rel="stylesheet" type="text/css" />
+<link href="css/style_web.css" rel="stylesheet" type="text/css" />
+<link href="css/style_contacts.css" rel="stylesheet" type="text/css" />
+<link href="css/style_about.css" rel="stylesheet" type="text/css" />
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+ <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+<script src="js/urlCheck.js"></script>
+<script src="js/anim.js"></script>
+<script src="js/anim_ulitka.js"></script>
+<script src="js/anim_hi.js"></script>
+<script src="js/cloud_identity_click.js"></script>
+<script src="js/cloud_poligraphy_click.js"></script>
+<script src="js/cloud_other_click.js"></script>
+<script src="js/cloud_web_click.js"></script>
+<script src="js/cloud_contacts_click.js"></script>
+<script src="js/cloud_about_click.js"></script>
+<script src="js/slider.js"></script>
+<link href="css/media-queries.css" rel="stylesheet" type="text/css">
+<script src="js/flip.js"></script>
+<script src="js/jquery.color.js"></script>
+<script src="js/rotate3Di.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+</head>
+
+<body>
+<div class="header">
+	<a id="ada" href="/zhenia"><div class="logo"><img src="img/logo.png" /></div></a>
+	<div class="privet">
+		<div class="hi2">
+			<img src="img/hi2.png" />
+		</div>
+		<div id="hi" class="hi">
+			
+		</div>
+		<div class="hi_sova">
+			
+		</div>
+	</div>	
+</div>
+<div class="cloud1">
+	<img class="c1" src="img/cloud1.png" />
+</div>
+<div class="cloud2">
+	<img class="c2" src="img/cloud2.png" />
+</div>
+<div class="cloud3">	
+	<img class="c3" src="img/cloud3.png" />
+</div>
+<div class="cloud4" id="cloud4">
+	<img class="c4" src="img/cloud6.png" />
+</div>
+<div class="cloud5" id="cloud5">
+	<img class="c5" src="img/cloud2.png" />
+</div>
+<div class="cloud6">
+	<img class="c6" src="img/cloud1.png" />
+</div>
+<div class="cloud7"></div>
+
+
+	<div class="znak">
+		<div class="about"></div>
+		<div class="bird"></div>
+		<div class="arrow"></div>
+	</div>
+	
+<div class="center">
+	<div class="portfolio" id="portfolio"></div>
+	<div class="contacts"></div>
+</div>
+<div class="ulitka">
+	<div id="roga"></div>
+	<div id="pancir"></div>
+	<div id="telo"></div>
+</div>
+<div class="bgslider">
+	<div id="slideshow">
+		<div id="slidesContainer">
+			<div class="slide">
+				<img src="img/1slide.png">
+			</div>
+			<div class="slide">
+				<img src="img/1slide.png">
+			</div>
+			<div class="slide">
+				2
+			</div>
+			<div class="slide">
+				3
+			</div>
+		</div>
+	</div>
+	<span class="control" id="leftControl"></span>
+	<span class="control" id="rightControl"></span>
+</div>
+</body>
+</html>
